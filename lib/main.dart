b06572c0df29de7a8cca18550fef51f593b436ef
richymaysoft <132938462+richymaysoft@users.noreply.github.com>
2024-03-06 14:18:47 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:figma_practice/screens/landing_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const ProviderScope(
     child: MaterialApp(
       home: LandingScreen(),
