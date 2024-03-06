6a668f0aadbc7d437d13e0d20b6079f3ffcf1025
richymaysoft <132938462+richymaysoft@users.noreply.github.com>
2024-03-06 14:20:02 -0800

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,7 @@
-import 'package:figma_practice/models/meal.dart';
-import 'package:figma_practice/riverpod/cart_provider.dart';
-import 'package:figma_practice/screens/checkout_address_screen.dart';
-import 'package:figma_practice/widgets/cart_item.dart';
+import '/models/meal.dart';
+import '/riverpod/cart_provider.dart';
+import '/screens/checkout2_address_screen.dart';
+import '/widgets/cart_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -58,97 +58,103 @@ class _CardScreenState extends ConsumerState<CardScreen> {
           ),
           textAlign: TextAlign.center,
         ),
-        backgroundColor: Color(0xFFF5F5F8),
+        backgroundColor: const Color(0xFFF5F5F8),
       ),
-      body: Container(
-        padding: const EdgeInsets.only(top: 50),
-        width: 414,
-        height: screenHeight * 0.9,
-        clipBehavior: Clip.antiAlias,
-        decoration: ShapeDecoration(
-          color: const Color(0xFFF5F5F8),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+      body: ListView(children: [
+        Container(
+          padding: const EdgeInsets.only(
+            top: 40,
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          //  width: 414,
+          //  height: screenHeight * 0.9,
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
+            color: const Color(0xFFF5F5F8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Icon(Icons.chevron_left_outlined),
-                Text(
-                  'swipe on an item to delete',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 13,
-                    fontFamily: 'SF Pro Text',
-                    fontWeight: FontWeight.w400,
-                    height: 0,
-                  ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.chevron_left_outlined),
+                    Text(
+                      'swipe on an item to delete',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                        fontFamily: 'SF Pro Text',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                  ],
                 ),
+                const SizedBox(
+                  height: 25,
+                ),
+                SizedBox(
+                  height: screenHeight * 0.6,
+                  child: cartMeals.isEmpty
+                      ? const Center(
+                          child: Text(
+                            'No item in Cart',
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                        )
+                      : SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              for (Meal meal in cartMeals)
+                                CartItem(
+                                  meal: meal,
+                                  onTotalCart: onTotalCart,
+                                  onCartSumUp: cartSumUp,
+                                )
+                            ],
+                          ),
+                        ),
+                ),
+                if (cartMeals.isNotEmpty)
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (ctx) =>
+                              CheckoutAddress(totalPrice: totalCartPrice)));
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(top: 45, bottom: 20),
+                      width: 314,
+                      height: 70,
+                      decoration: ShapeDecoration(
+                        color: const Color(0xFFFA4A0C),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        'Complete order',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFFF6F6F9),
+                          fontSize: 17,
+                          fontFamily: 'SF Pro Text',
+                          fontWeight: FontWeight.w600,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ),
               ],
             ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              height: screenHeight * 0.6,
-              child: cartMeals.isEmpty
-                  ? const Center(
-                      child: Text(
-                        'No item in Cart',
-                        style: TextStyle(fontSize: 20, color: Colors.black),
-                      ),
-                    )
-                  : SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          for (Meal meal in cartMeals)
-                            CartItem(
-                              meal: meal,
-                              onTotalCart: onTotalCart,
-                              onCartSumUp: cartSumUp,
-                            )
-                        ],
-                      ),
-                    ),
-            ),
-            if (cartMeals.isNotEmpty)
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (ctx) =>
-                          CheckoutAddress(totalPrice: totalCartPrice)));
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(top: 45),
-                  width: 314,
-                  height: 70,
-                  decoration: ShapeDecoration(
-                    color: Color(0xFFFA4A0C),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: const Text(
-                    'Complete order',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFFF6F6F9),
-                      fontSize: 17,
-                      fontFamily: 'SF Pro Text',
-                      fontWeight: FontWeight.w600,
-                      height: 0,
-                    ),
-                  ),
-                ),
-              ),
-          ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
